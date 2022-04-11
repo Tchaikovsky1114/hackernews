dea0ab8470a83701835c2be5730db56bf0700f6e
Kim Myungseong <forwarm5891@gmail.com>
2022-04-11 17:27:27 +0900

    feat: Implament Feeds and Details page

---
+++ index.ts
@@ -0,0 +1,8 @@
+
+type Centimeter = Number;
+type RainbowColor = 'red' | 'orange' | 'yellow' | 'green' | 'blue'
+let age = 10;
+let weight:number = 80;
+let height:Centimeter = 176;
+let color:RainbowColor = 'red';
+
